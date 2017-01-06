5d867407e4aa6f126d3a6c1a0c4ded028686a15d
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2017-01-06 18:02:53 +0100

    Render table of random movies from the api

--- src/app.js
+++ src/app.js
@@ -12,14 +12,48 @@ userInputStream$.subscribe(input => {
     label.textContent = `Input: ${input}`;
 });
 
-// ########### Promise Stream ###########
+// ########### Promise stream ###########
 const postsPromise = fetch('http://swapi.co/api/films/')
     .then(data => data.json())
     .catch(err => console.error(err));
 
-const responseStream = Rx.Observable.fromPromise(postsPromise);
+const responseStream$ = Rx.Observable.fromPromise(postsPromise)
+    .map(data => data.results);
 
-responseStream.subscribe(data => console.log(data), err => console.error(err));
+function createSuggestions(responseStream){
+    return responseStream.map(films =>
+        films[Math.floor(Math.random()*films.length)]
+    );
+}
+
+function renderSuggestion(filmData, selector){
+    const element = document.querySelector(selector);
+    const title = element.querySelector('.title');
+    const director = element.querySelector('.director');
+    const releaseDate = element.querySelector('.release-date');
+
+    title.textContent = filmData.title;
+    director.textContent = filmData.director;
+    releaseDate.textContent = filmData.release_date;
+}
+
+const suggestion1Stream$ = createSuggestions(responseStream$);
+const suggestion2Stream$ = createSuggestions(responseStream$);
+const suggestion3Stream$ = createSuggestions(responseStream$);
+
+suggestion1Stream$.subscribe(film => {
+    renderSuggestion(film, '.suggestion1');
+});
+
+suggestion2Stream$.subscribe(film => {
+    renderSuggestion(film, '.suggestion2');
+});
+
+suggestion3Stream$.subscribe(film => {
+    renderSuggestion(film, '.suggestion3');
+});
+
+responseStream$.subscribe(data => console.log(data), err => console.error(err));
 
 
 
