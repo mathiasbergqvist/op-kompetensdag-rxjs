d55f5ba645841333cadca00ca55d8d39f1274b33
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2017-01-06 21:04:43 +0100

    Update the dom from subscription to scoreStream

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const Rx = require('rxjs/Rx');
 // ########### Event stream ###########
 const stormtrooperCounter = document.querySelector('#stormtrooper-counter');
 const wookieCounter = document.querySelector('#wookie-counter');
-const jediScore = document.querySelector('#jedi-score');
-const jediVerdict = document.querySelector('#jedi-verdict');
+const stormtrooperAmount = document.querySelector('#stormtrooper-amount');
+const wookieAmount = document.querySelector('#wookie-amount');
+const jediScoreHolder = document.querySelector('#jedi-score');
+const jediVerdictHolder = document.querySelector('#jedi-verdict');
 
 const stormtrooperInputStream$ = Rx.Observable.fromEvent(stormtrooperCounter, 'input')
     .map(event => event.target.value);
@@ -19,13 +21,41 @@ const scoreStream$ = Rx.Observable.combineLatest(
     (stormtrooperCount, wookieCount) => {
         const stormtrooperScore = stormtrooperCount * 0.7;
         const wookieScore = wookieCount * 1.3;
-        const jediScore = stormtrooperScore - wookieScore;
-        return {stormtrooperScore, wookieScore, jediScore};
+        const jediScore = Math.round(stormtrooperScore - wookieScore);
+        return {stormtrooperCount, wookieCount, jediScore};
     }
 )
 
+function getJediVerdict(jediScore){
+    if(jediScore < -30){
+        return "I find your lack of faith disturbing.";
+    }
+    if(jediScore > -30 && jediScore <= 0){
+        return "It's a trap!";
+    }
+    else if(jediScore > 0 && jediScore <= 20){
+        return "Do. Or do not. There is no try.";
+    }
+    else if(jediScore > 20 && jediScore <= 40){
+        return "Never tell me the odds.";
+    }
+    else if(jediScore > 40 && jediScore <=60){
+        return "Great, kid. Don’t get cocky.";
+    }
+    else {
+        return "The force is strong in this one!";
+    }
+}
+
 scoreStream$.subscribe(score => {
-    console.log("scores", score)
+    const {stormtrooperCount, wookieCount, jediScore} = score;
+    let jediVerdict = getJediVerdict(jediScore);
+    stormtrooperAmount.textContent = `#${stormtrooperCount}`;
+    stormtrooperCounter.value = score.stormtrooperCount;
+    wookieAmount.textContent = `#${wookieCount}`;
+    wookieCounter.value = score.wookieCount;
+    jediScoreHolder.textContent = `Jedi score: ${jediScore}`;
+    jediVerdictHolder.textContent = `Jedi verdict: ${jediVerdict}`;
 });
 
 
@@ -44,7 +74,6 @@ function createSuggestions(responseStream){
 }
 
 function renderSuggestion(heroData, selector){
-
     const element = document.querySelector(selector);
     const name = element.querySelector('.name');
     const birthYear = element.querySelector('.birth-year');
