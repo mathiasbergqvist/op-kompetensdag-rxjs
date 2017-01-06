9ee678ec3fadd440666648d01439c1bedd493f6e
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2017-01-06 17:30:08 +0100

    Setup promise stream to fetch data from StarWars api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require("./style.css");
 const Rx = require('rxjs/Rx');
 
-// ########### Input stream
+// ########### Event stream ########### 
 const userInput = document.querySelector('#user-input');
 const label = document.querySelector('#label');
 
@@ -11,4 +11,17 @@ const userInputStream$ = Rx.Observable.fromEvent(userInput, 'input')
 userInputStream$.subscribe(input => {
     label.textContent = `Input: ${input}`;
 });
+
+// ########### Promise Stream ###########
+const postsPromise = fetch('http://swapi.co/api/films/')
+    .then(data => data.json())
+    .catch(err => console.error(err));
+
+const responseStream = Rx.Observable.fromPromise(postsPromise);
+
+responseStream.subscribe(data => console.log(data), err => console.error(err));
+
+
+
+
 
